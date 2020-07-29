d83e086ada025d22390c9d64e94fe966223d3e06
SchnoppDog <schlagerclaus90@gmail.com>
2020-07-29 15:53:31 +0200

    Removing console.logs

--- backend/lib/expModules/lightStrip.js
+++ backend/lib/expModules/lightStrip.js
@@ -37,7 +37,6 @@ exports.lightOn = function(keyNote,r,g,b,a) {
 exports.lightOff = function(keyNote, options) {
   let r,g,b,a
   let durationTime
-  // console.log("before if: ", options.freezeOpts.duration)
     if(options) {
       if(options.isFreeze === 'true') {
         r        = options.freezeOpts.rgba.red
@@ -46,14 +45,11 @@ exports.lightOff = function(keyNote, options) {
         a        = options.freezeOpts.rgba.alpha
         durationTime = options.freezeOpts.duration
         freeze   = true
-        // console.log("inside if: ", options.freezeOpts.duration)
       } else {
         freeze = false
         durationTime = 0
       }
     }
-    // console.log("after if: ", options.freezeOpts.duration)
-    // console.log("durationTime: ", durationTime)
     const ledNum1 = keyNote - keyLed1 + (keyNote - keyLed2)
     const ledNum2 = keyNote - keyLed1 + (keyNote - keyLed1)
     if(keyNote >= firstRangeKey && keyNote <= lastRangeKey) {
